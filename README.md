f7840432a298a9856f12e1017ffa80286160091d
Sebastian Holager <bassetassen@hotmail.com>
2014-11-10 21:48:12 +0100

    added filter

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-crmsvcutilextensions
+CRMSvcUtilExtensions
 ====================
+
+An extension to only generate standard entities and attributes.
+
+Copy crmsvcutilextensions to same folder as CrmSvcUtil. Call CrmSvcUtil with /codefilter parameter
+
+'''
+CrmSvcUtil /codewriterfilter:"CRMSvcUtilExtensions.VanillaFilter,CRMSvcUtilExtensions" /url:... /out:D:\Entities.cs /username:... /password:... /namespace:...
+'''
---
+++ src/CRMSvcUtilExtensions/VanillaFilter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.Crm.Services.Utility;
+using Microsoft.Xrm.Sdk.Metadata;
+
+namespace CRMSvcUtilExtensions
+{
+    public class VanillaFilter : ICodeWriterFilterService
+    {
+        private readonly ICodeWriterFilterService defaultService;
+
+        public VanillaFilter(ICodeWriterFilterService defaultService)
+        {
+            this.defaultService = defaultService;
+        }
+
+        public bool GenerateOptionSet(OptionSetMetadataBase optionSetMetadata, IServiceProvider services)
+        {
+            return defaultService.GenerateOptionSet(optionSetMetadata, services);
+        }
+
+        public bool GenerateOption(OptionMetadata optionMetadata, IServiceProvider services)
+        {
+            return defaultService.GenerateOption(optionMetadata, services);
+        }
+
+        public bool GenerateEntity(EntityMetadata entityMetadata, IServiceProvider services)
+        {
+            if (entityMetadata.IsCustomEntity.HasValue && entityMetadata.IsCustomEntity.Value)
+                return false;
+
+            return defaultService.GenerateEntity(entityMetadata, services);
+        }
+
+        public bool GenerateAttribute(AttributeMetadata attributeMetadata, IServiceProvider services)
+        {
+            if (attributeMetadata.IsCustomAttribute.HasValue && attributeMetadata.IsCustomAttribute.Value)
+                return false;
+
+            return defaultService.GenerateAttribute(attributeMetadata, services);
+        }
+
+        public bool GenerateRelationship(RelationshipMetadataBase relationshipMetadata, EntityMetadata otherEntityMetadata,
+            IServiceProvider services)
+        {
+            if (relationshipMetadata.IsCustomRelationship.HasValue && relationshipMetadata.IsCustomRelationship.Value)
+                return false;
+
+            return defaultService.GenerateRelationship(relationshipMetadata, otherEntityMetadata, services);
+        }
+
+        public bool GenerateServiceContext(IServiceProvider services)
+        {
+            return defaultService.GenerateServiceContext(services);
+        }
+    }
+}
